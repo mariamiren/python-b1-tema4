7c2a73c869b2ec3085cf654e62854e635ab142cb
mariamiren <mcarmona8@uoc.edu>
2025-12-04 20:03:46 +0100

    Update ej4b3.py

--- 4b/ej4b3.py
+++ 4b/ej4b3.py
@@ -80,12 +80,80 @@ def create_list(length_list):
     Returns:
     A tuple containing two lists of integers, the first one created in RAM and
     the second one created in Heap by reusing the first list.
-    """
+  """
+    
+def test_create_list_returns_tuple_of_two_lists():
+    
+    #GIVEN
+    length_list = 4
+    
+    #WHEN
+    result = create_list(length_list)
+    
+    #THEN
+    assert insinstance(result, tuple)
+    assert len(result) == 2,
+    assert insinstance(result[0], list),
+    assert insinstance(result[1], list),
 
-    # Write here your code
-    pass
+def test_create_list_lits_have_same_length():
 
+    #GIVEN
+    length_list = 6
+    
+    #WHEN
+    ram_list, heap_list = create_list(length_list)
 
+    #THEN
+    assert len(ram_list) == len(heap_list)
+
+def test_create_list_ram_list_contains_random_integers():
+
+    #GIVEN
+    length_list = 7
+
+    #WHEN
+    ram_list, _ = create_list(length_list)
+    
+    #THEN
+    assert all(insinstance(i, int) for i in ram_list), "the first list returned by create_list should contain only integers"
+    assert all(0<=i<=100 for i in ram_list), "the first list returned by create_list should contain only integers between 0 and 100"
+
+def test_create_list_heap_list_is_a_copy_of_ram_list():
+
+    #GIVEN
+    length_list = 8
+
+    #WHEN
+    ram_list, heap_list = create_list(length_list)
+
+    #THEN
+    assert ram_list == heap_list, "the second list returned by create_list should be a copy of the first list"
+    assert id(ram_list) != id(heap_list), "the second list returned by create_list should be a copy of the first list"
+
+def test_create_list_with_zero_lentg():
+
+    #GIVEN
+    length_list = 0
+
+    #WHEN
+    ram_list, heap_list = create_list(length_list)
+
+    #THEN
+    assert len(ram_list) == 0, "the first list returned by create_list should be empty for lentg_list 0"
+    assert len(heap_list) == 0, "the second list returned by create_list should be empty for lentg_list 0"    
+
+def test_create_list_with_negative_length():
+
+     #GIVEN
+    length_list = -5
+
+    #WHEN / THEN
+    with pytest.raises(ValueError):
+        create_list(length_list)
+    
+    
+    
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # print(create_list(6))
