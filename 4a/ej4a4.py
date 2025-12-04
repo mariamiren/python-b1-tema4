4ae359d626a635bf3ef7d4b2f5c9df3396a4e1ef
mariamiren <mcarmona8@uoc.edu>
2025-12-04 18:48:30 +0100

    Update ej4a4.py

--- 4a/ej4a4.py
+++ 4a/ej4a4.py
@@ -30,8 +30,41 @@ Enunciat:
 
 
 def add_elapsed_time(dictionary, key, value):
-    # Write here your code
-    pass
+    
+    #Test con una lista vacía
+    assert add_elapsed_time({}, "Juan", 8.6) == {"Juan": 8.6}, "add_elapsed_time does not return the correct value for input {}, 'Juan', 8.6. It should be {'Juan': 8.6}"
+    
+    
+    #Test con un diccionario que ya tiene elementos
+    my_dict = {"Juan": 9.5, "Peter": 14.2, "Sofia": 6.5, "Alex": 8.7}
+    assert add_elapsed_time(my_dict, "Juan", 8.6) == {
+        "Juan": 8.6,
+        "Peter": 14.2,
+        "Sofia": 6.5,
+        "Alex": 8.7,
+    }, "add_elapsed_time does not return the correct value for input {'Juan': 9.5, 'Peter': 14.2, 'Sofia': 6.5, 'Alex': 8.7}, 'Juan', 8.6. It should be {'Juan': 8.6, 'Peter': 14.2, 'Sofia': 6.5, 'Alex': 8.7}"
+    
+    
+    #Test con una clave nueva
+    my_dict = {"Juan": 9.5, "Peter": 14.2, "Sofia": 6.5, "Alex": 8.7}
+    assert add_elapsed_time(my_dict, "Lucas", 11.2) == {
+        "Juan": 8.6,
+        "Peter": 14.2,
+        "Sofia": 6.5,
+        "Alex": 8.7,
+        "Lucas": 11.2,
+    }, "add_elapsed_time does not return the correct value for input {'Juan': 9.5, 'Peter': 14.2, 'Sofia': 6.5, 'Alex': 8.7}, 'Lucas', 11.2. It should be {'Juan': 9.5, 'Peter': 14.2, 'Sofia': 6.5, 'Alex': 8.7, 'Lucas': 11.2}"
+    
+    
+    #Test con un valor actualizado
+     my_dict = {"Juan": 9.5, "Peter": 14.2, "Sofia": 6.5, "Alex": 8.7}
+    assert add_elapsed_time(my_dict, "Juan", 10.2) == {
+        "Juan": 10.2,
+        "Peter": 14.2,
+        "Sofia": 6.5,
+        "Alex": 8.7,
+    }, "add_elapsed_time does not return the correct value for input {'Juan': 9.5, 'Peter': 14.2, 'Sofia': 6.5, 'Alex': 8.7}. It should be {'Juan': 10.2, 'Peter': 14.2, 'Sofia': 6.5, 'Alex': 8.7}"
+        
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
