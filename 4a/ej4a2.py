c969b33cdb7aa24a8f045395180b3c805197cc0e
mariamiren <mcarmona8@uoc.edu>
2025-12-04 17:52:16 +0100

    Update ej4a2.py

--- 4a/ej4a2.py
+++ 4a/ej4a2.py
@@ -44,8 +44,24 @@ Exemple:
 
 
 def count_fruits(fruits_list):
-    # Write here your code
-    pass
+   fruits = [
+       "apple",
+       "banana",
+       "orange",
+       "apple",
+       "kiwi",
+       "banana",
+       "kiwi",
+       "kiwi",
+       "kiwi",
+   ]
+    assert count_fruits(fruits) == {"apple": 2}, {"banana": 2}, {"orange": 1}, {"kiwi": 4}, "count_fruits does not return the correct value for the input ['apple', 'banana', 'orange', 'apple', 'kiwi', 'banana', 'kiwi', 'kiwi', 'kiwi']. It should be {'apple': 2, 'banana': 2, 'orange': 1, 'kiwi': 4}" 
+    fruits = []
+    assert count_fruits(fruits) == {}, "count fruits does not return the correct value for input []. It should be {}, that is, empty"
+    fruits == ["apple", "apple", "apple", "apple"]
+    assert count_fruits(fruits) == {"apple": 4}, "count fruits does not return the correct value for input ['apple', 'apple', 'apple', 'apple']. It shoul be {'apple': 4}"
+    fruits == ["apple", "banana", "kiwi"]
+    assert count_fruits(fruits) == {"apple": 1, "banana": 1, "kiwi":1}, "count fruits does not return the correct value for input ['apple', 'banana', 'kiwi']. It should be {'apple': 1, 'banana': 1, 'kiwi':1}"
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
