38a5a2274d1da77aea283e5277314557fe45b35b
mariamiren <mcarmona8@uoc.edu>
2025-12-04 18:15:43 +0100

    Update ej4a3.py

--- 4a/ej4a3.py
+++ 4a/ej4a3.py
@@ -37,10 +37,22 @@ Exemple:
 
 
 def descending_list_iterator(numbers_list):
-    # Write here your code
-    pass
+    
+    #test with empty list
+    assert descending_list_iterator([])) == [], "descending_list_iterator does not return the correct value for input []. It sould be [], that is, empty"
+    
+    #test with a list of one element
+    assert descending_list_iterator([7]) == [7], "descending_list_iterator does not return the correct value for input [7]. It sould be [7]"
 
+    #test with a list of multiple elements
+    assert descending_list_iterator([7, 3, 5, 8]) == [7, 2, 1, 5], "descending_list_iterator does not return the correct value for input [7, 3, 5, 8]. It sould be [7, 2, 1, 5]"
 
+    #test with a list of repeated elements
+    assert descending_list_iterator([1, 1, 1, 1]) == [1, 1, 1, 1], "descending_list_iterator does not return the correct value for input [1, 1, 1, 1]. It sould be [1, 1, 1, 1]"
+    
+    #test with a list of negative elements
+    assert descending_list_iterator([-1, -5, -3, -2]) == [-5, -4, -3, -2], "descending_list_iterator does not return the correct value for input [-1, -5, -3, -2]. It sould be [-5, -4, -3, -2]"
+    
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
