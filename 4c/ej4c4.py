c2adef538883c131b1dfc410a7763701d5ce771a
mariamiren <mcarmona8@uoc.edu>
2025-12-04 21:59:23 +0100

    Update ej4c4.py

--- 4c/ej4c4.py
+++ 4c/ej4c4.py
@@ -78,7 +78,17 @@ class Parrot():
     def talk(self):
         pass
 
-
+from ej4c4.py import Dog, Cat, Parrot
+	def test_talk_animals():
+		perro = Dog("Fido")
+		gato = Cat("Fido")
+		loro = Parrot("Polly")
+		assert perro.talk() == "¡Guau!", "Dog talk does not return the correct value for input 'Fido'. It should be '¡Guau!'"
+		assert gato.talk() ==  "¡Meow!", "Cat talk does not return the correct value for input 'Fido'. It should be '¡Meow!'"
+		assert loro.talk() == "¡Whistle!", "Parrot talk does nor return the correct value for input 'Polly'. It should be '¡Whistle!'"
+		assert perro.name == "Fido", "Dog name does not return the correct value for input 'Fido'. It should be 'Fido'"
+		assert gato.name == "Fido", "Cat name does not return the correct value for input 'Fido'. It should be 'Fido'"
+		assert loro. name == "Polly", "Parrot name does not return the correct value for input 'Polly'. It should be 'Polly'"
 
 
 
